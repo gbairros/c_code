bff76b472d742c9816f51415deee0f2e0e4e8842
Gildo <gbairros@gmail.com>
2023-10-11 20:30:49 -0300

    Funcoes de String - Conta Caracteres

---
+++ strings/conta_caracteres.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	char frase[500+1];
+	printf("Informe uma palavra ");
+	scanf("%s", frase);
+	int i=0;
+	
+	while(frase[i] != '\0'){
+		i++;
+	}
+	printf("A frase %s tem %d\n", frase, i);
+	return 0;
+}
+	
